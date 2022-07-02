dea49636ab32072680efb457ba5096b96c8c37bd
imcloudlaunch <Opeyemikomolafe682@gmail.com>
2022-07-01 18:49:41 -0700

    6-print_numberz.c Emarv

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
   */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar(n);
-        }
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }

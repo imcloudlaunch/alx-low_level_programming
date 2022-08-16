2293b4c0c1aba01c38cb60b1f9a627a853aebb60
imcloudlaunch <Opeyemikomolafe682@gmail.com>
2022-08-16 13:39:47 -0700

    Empty

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * find_listint_loop - fills memory with a constant byte
+  * @head: is the owner of the dog
+  * Return: a number
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *headcopy;
+
+	headcopy = head;
+	return (headcopy);
+}

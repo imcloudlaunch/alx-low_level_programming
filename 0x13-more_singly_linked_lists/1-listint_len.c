d97492f91d2b4b431865965802331b70c9d22e82
imcloudlaunch <Opeyemikomolafe682@gmail.com>
2022-08-16 13:06:11 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+  * listint_len - fills memory with a constant byte
+  * @h: is the owner of the dog
+  * Return: a number
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}

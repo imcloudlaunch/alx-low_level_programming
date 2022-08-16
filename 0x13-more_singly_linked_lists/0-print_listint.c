f368667fae0e44e3a10fb7a3655cbcf5d050b257
imcloudlaunch <Opeyemikomolafe682@gmail.com>
2022-08-16 12:59:46 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * print_listint - fills memory with a constant byte
+  * @h: is the owner of the dog
+  * Return: a number
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}

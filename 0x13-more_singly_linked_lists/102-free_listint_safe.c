c10fa8f2465d520286ac3e661fbb83f8d670f232
imcloudlaunch <Opeyemikomolafe682@gmail.com>
2022-08-16 13:37:07 -0700

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * free_listint_safe - fills memory with a constant byte
+  * @h: is the owner of the dog
+  * Return: a number
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i = 0;
+
+	(void) h;
+	return (i);
+}

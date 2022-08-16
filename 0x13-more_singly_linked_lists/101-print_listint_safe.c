dea69645dee36cff8cfb717bc2a6c65770eff587
imcloudlaunch <Opeyemikomolafe682@gmail.com>
2022-08-16 13:34:59 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * print_listint_safe - fills memory with a constant byte
+  * @head: is the owner of the dog
+  * Return: a number
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+
+	(void) head;
+	return (i);
+}
